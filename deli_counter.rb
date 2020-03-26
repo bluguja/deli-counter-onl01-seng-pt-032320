ecfba801de18e9453db5d43aaeb4dc43a54807c8
Bala Luguja <bluguja@gmail.com>
2020-03-26 00:12:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -43,7 +43,7 @@ def now_serving(line)
   
  if line.length == 0 
  
-    p"There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line.first}." 
     line.shift 
